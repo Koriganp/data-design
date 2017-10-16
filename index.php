5972e63c45a1ac5d21823cc792423dba15daf317
Korigan Payne <koriganp@gmail.com>
2017-10-15 18:32:19 -0600

    Added img, some content, style, and meta tags

--- index.php
+++ index.php
@@ -1,7 +1,25 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: korig
- * Date: 10/12/2017
- * Time: 4:43 PM
- */
+<!DOCTYPE html>
+<html lang="en-US">
+	<head>
+		<meta charset="utf-8"/>
+		<meta name="author" content="Korigan Payne"/>
+		<meta name="description" content=""/>
+		<link href="styles/style.css" rel="stylesheet" type="text/css"/>
+		<link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"/>
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<main>
+			<h1>Persona</h1>
+			<img id="jared" src="images/persona.jpg" alt="Picture of Jared Mason"/>
+			<p>Jared Mason is a 32 year old businessman who works for a large marketing firm. His work requires him to spend
+			several days a week in different cities across the US, and sometimes overseas. He loves his job, and enjoys helping
+			his clients reach the new customers in markets that they normally wouldn't appeal to. He is a real go-getter, with
+			a no-nonsense attitude.</p>
+			<p>Jared also enjoys taking his clients out to extravagant places to eat, because he is also a foodie. He loves fine
+			dining as well as the low-key places that no one really knows about but the food loving community.</p>
+			<h1>User Story</h1>
+			<h1>Use Case/Interaction Flow</h1>
+		</main>
+	</body>
+</html>
