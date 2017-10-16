dd1e4ba2d1edb0dec151a2c762181d6721c41901
Korigan Payne <koriganp@gmail.com>
2017-10-15 18:58:01 -0600

    Completed persona, user story, and case/interaction flow

--- index.php
+++ index.php
@@ -17,9 +17,35 @@
 			his clients reach the new customers in markets that they normally wouldn't appeal to. He is a real go-getter, with
 			a no-nonsense attitude.</p>
 			<p>Jared also enjoys taking his clients out to extravagant places to eat, because he is also a foodie. He loves fine
-			dining as well as the low-key places that no one really knows about but the food loving community.</p>
+			dining as well as the low-key places that no one really knows about but the food loving community. Jared also likes
+			night life and enjoys meeting new people and having a good time.</p>
+
 			<h1>User Story</h1>
+			<p>Because Mr. Mason is often in places that he hasn't been or has only been to a few times, he wants a way to
+			communicate with other food enthusiasts in what ever city he is traveling to that week. He wants to find locations
+			to treat his clients to that they might not even know about. He wants to get information about the restaurants before
+			he arrives and be able to inspect the establishment before he takes his clients there. He wants something that would
+			provide both user insight and opinions as well as reviews of the place recommended.</p>
+			<p>Jared is also looking for a place to meet people that share his interests when he is not working. He wants to
+			know where the classier places to frequent are, and if there is an opinion of the residents of the city which is
+			the best for his personality and interests.</p>
+
 			<h1>Use Case/Interaction Flow</h1>
+			<ul>
+				<li>Jared goes to the reddit website</li>
+				<li>Site prompts user to login</li>
+				<li>Jared enters his username and password</li>
+				<li>Site redirects to Jared's user page with search bar</li>
+				<li>Jared types in the city he is traveling to</li>
+				<li>Site goes to page of that city</li>
+				<li>Jared creates a post asking where the best places to dine are</li>
+				<li>User X comments on his post</li>
+				<li>User Y comments on User X's comment</li>
+				<li>User Z comments on Jared's post</li>
+			</ul>
+
+			<h1>Conceptual Model</h1>
+
 		</main>
 	</body>
 </html>
